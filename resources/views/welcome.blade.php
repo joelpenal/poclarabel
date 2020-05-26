20ab53667c2d9716fd71246068a8e25227bf11df
joelpenal <joel.pena.lopez@gmail.com>
2020-05-26 11:55:18 -0400

    add text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Club Piscine
+                    Club Piscine Super Fitness
                 </div>
 
                 <div class="links">
